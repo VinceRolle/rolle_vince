503ba0d623df1fbc70cea87f0e21f1add24db946
VinceRolle <rollevince95@gmail.com>
2025-09-14 18:17:20 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
-    'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'hostname'	=> getenv("sql12.freesqldatabase.com"),
+    'port'		=> getenv("3306"),
+    'username'	=> getenv("sql12798479"),
+    'password'	=> getenv("5xl3AsWLu9"),
+    'database'	=> getenv("sql12798479"),
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
